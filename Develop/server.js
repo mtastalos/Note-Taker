e3c088df3e46135bb8cf0fccfa49fca6e9b936b2
Michael Astalos <michaeldont@gmail.com>
2021-09-03 22:15:18 -0400

    writting to db.json, not working on reading data

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path')
+const fs = require('fs');
+const noteArray = require('./db/db')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,12 +15,24 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// })
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/notes', (req, res) => {
+    console.log(noteArray)
+    res.json(noteArray);
+})
+
+app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    noteArray.push(note);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(noteArray), null, 2);
+    return noteArray;
 })
 
 app.listen(PORT, () => {
